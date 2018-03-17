42ca40dcfd97bb774022ba69bd0fba95f99bb6c0
leilei <thunderbey@163.com>
2018-03-17 10:22:33 +0800

    feat:正式开始提交，1.0版本 完成基础读写文件功能

--- Word_Count/src/Test.java
+++ Word_Count/src/Test.java
@@ -1,4 +1,9 @@
-
+/*
+ * 程序：word_count
+ * 当前功能：读写文件
+ * 创作者：雷佳谕
+ * 版本：1.0
+ */
     import java.io.File;  
     import java.io.InputStreamReader;  
     import java.io.BufferedReader;  
@@ -14,28 +19,27 @@ public static void main(String[] args)
     { // 防止文件建立或读取失败，用catch捕捉错误并打印，也可以throw  
 
         /* 读入TXT文件 */  
-        String pathname = "test.txt"; // 绝对路径或相对路径都可以，这里是绝对路径，写入文件时演示相对路径  
+        String pathname = "file.c";   // 使用相对路径
         File filename = new File(pathname); // 要读取以上路径的input.txt文件  
-        InputStreamReader reader = new InputStreamReader(  
-                new FileInputStream(filename)); // 建立一个输入流对象reader  
+        InputStreamReader reader = new InputStreamReader( new FileInputStream(filename)); // 建立一个输入流对象reader  
         BufferedReader br = new BufferedReader(reader); // 建立一个对象，它把文件内容转成计算机能读懂的语言  
         String line = "";  
-        line = br.readLine();  
-        System.out.println(line);
-        while (line != null) {  
+        line = br.readLine();  //读取文件第一行
+        System.out.println(line); //测试读取内容
+        while (line != null) 
+        {  
             line = br.readLine(); // 一次读入一行数据 
             if(line!=null)
             System.out.println(line);
         }  
 
-        /* 写入Txt文件 */  
+        /* 写入文件 */  
         File writename = new File("output.txt"); // 相对路径，如果没有则要建立一个新的output。txt文件  
         writename.createNewFile(); // 创建新文件  
         BufferedWriter out = new BufferedWriter(new FileWriter(writename));  
         out.write("我会写入文件啦\r\n"); // \r\n即为换行  
         out.flush(); // 把缓存区内容压入文件  
         out.close(); // 最后记得关闭文件  
-
     } 
     catch (Exception e) 
     {  
