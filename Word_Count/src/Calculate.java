413543106febd7d3bf1cc01b9d8903fd8fe8c67d
leilei <thunderbey@163.com>
2018-03-18 21:17:56 +0800

    feat:完成停用词表功能，5.0版本 将之前很多分散的方法集成到一个函数中，已完成三项基本功能及停用词表，递归查找两项扩展功能 ‘ ’

--- Word_Count/src/Calculate.java
+++ Word_Count/src/Calculate.java
@@ -3,8 +3,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.io.DataOutputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculate    //计算文件总字符数，总单词数
 {
@@ -12,7 +20,9 @@ public class Calculate    //计算文件总字符数，总单词数
 	public int character;
 	public int word;
 	public int lines;
-public void calfile(String filepath)  //计算文件总字符数
+	
+/**无禁用词表情况下统计文件字符数单词数函数**/
+public void calfile(String filepath)  
 {
 	character=word=lines=0; //初始化
 	 try 
@@ -43,15 +53,15 @@ public void calfile(String filepath)  //计算文件总字符数
 }
 
 
-
-public int calLineWord(String line)   //统计单行单词数
+/**统计单行单词数函数**/
+public int calLineWord(String line)   
 {
 	int flag=1;
 	int lineword=0; //每行单词数
 	  for(int i = 0; i < line.length(); i++) 
 	  {  
           //System.out.println(str.charAt(i));  
-		  if(line.charAt(i)==' '|line.charAt(i)==','|line.charAt(i)=='，')
+		  if(line.charAt(i)==' '|line.charAt(i)==','|line.charAt(i)=='，'|line.charAt(i)=='\t')
 		  {
 			  flag=1;
 		  }
@@ -67,6 +77,81 @@ public int calLineWord(String line)   //统计单行单词数
     return lineword;
 }
 
+/**有禁用词表情况下统计文件字符数单词数函数**/
+public void s_calfile(String filepath,String stoppath)
+{
+	character=word=lines=0;
+	try 
+	{
+	 File file=new File(filepath); //要读取的文件
+	 File stop=new File(stoppath); //停用词表文件
+	 BufferedReader br = new BufferedReader(new FileReader(stop));
+	 BufferedReader br2 = new BufferedReader(new FileReader(file));
+	 String line = null;  //文件读取行
+	
+	 /**制作停用词表树**/
+	 //定义一个map集合保存停用单词
+	 TreeMap<String,Integer> tm = new TreeMap<String,Integer>();
+	 while((line=br.readLine())!=null)
+	 { 
+	    line.toLowerCase(); //转换为小写
+	 //将读到的字符串分割为单词
+	    String str[] = line.split(",+|\\s+|，+");
+	    //遍历得到的单词
+	    for(String s: str)
+	    {
+	    	if(!s.matches(""))
+	     {		
+	      //判断集合中是否已经存在该单词，如果存在则个数加一，否则将单词添加到集合中，且个数置为1
+	       System.out.println(s);
+	       if(!tm.containsKey(s))
+	       {
+	         tm.put(s,1);
+	       }
+	       else
+	       {
+	         tm.put(s,tm.get(s)+1);
+	       }
+	     }
+	    }
+	}
+	 //System.out.println(tm);
+	/**制作完毕**/
+	 System.out.println("以上为停用词表单词");
+	/**读取文件，统计单词数**/ 
+	 while((line=br2.readLine())!=null)
+	 { 
+		int temp=0;               //统计单行单词
+		character+=line.length(); //统计字符
+		lines++;                  //统计行数
+	    line.toLowerCase();      //转换为小写
+	    String str[] = line.split(",+|\\s+|，+");
+	    for(String s: str)
+	    {
+	    	if(!s.matches(""))
+	     {		
+	      //判断集合中是否已经存在该单词，如果存在则个数加一，否则将单词添加到集合中，且个数置为1
+	       if(!tm.containsKey(s))
+	       {
+	    	   System.out.println(s);
+	    	   temp++;
+	       }
+	     }
+	    }
+	    word+=temp;
+	}
+	/**统计完毕**/
+	 System.out.println("以上为去掉停用词表单词后的剩余单词");
+	}
+    catch (Exception e) 
+    {  
+        e.printStackTrace();  
+    } 
+	
+}
+
+/**清除文件内容函数**/
+/**在主函数中调用，用于清空已存在的输出文件的内容**/
 public void clearfile(String outpath)
 {
 	try 
@@ -74,25 +159,25 @@ public void clearfile(String outpath)
 		FileOutputStream out = new FileOutputStream(outpath,false); 
 		out.write(new String("").getBytes()); 
 		out.close(); 
-		System.out.println( "清空发送邮件日志成功!");
 	} 
 	catch (Exception ex) 
 	{ 
-		System.out.println("清空文件的内容失败,因为没有发送邮件日志文件!"); 
+		//System.out.println("清空失败!"); 
 	} 
 }
 
-public void writefile(File file,Calculate cal,String outpath)
+/**文件写函数**/
+public void writefile(File file,Calculate cal,String outpath)  
 {
 	try
 	{
 		/* 写入文件 */  
         File output = new File(outpath); // 相对路径，如果没有则要建立一个新的输出文件
-        if (!output.exists()) 
+        if (!output.exists())            
         {    
             output.createNewFile();// 不存在则创建    
-        }  
-        BufferedWriter out = new BufferedWriter(new FileWriter(output,true));  
+        } 
+        BufferedWriter out = new BufferedWriter(new FileWriter(output,true));  //实现追加
         out.write(file.getName()+":\r\n"+"字符数:"+cal.character+"\r\n"+"单词数:"+cal.word+"\r\n"+"行数:"+cal.lines+"\r\n"); // \r\n即为换行  
         out.flush(); // 把缓存区内容压入文件  
         out.close(); // 最后记得关闭文件  
@@ -103,19 +188,5 @@ public void writefile(File file,Calculate cal,String outpath)
 	    } 	
 }
 
-
-public boolean match(String filename,String temp) //对比文件名是否符合检索要求
-{
-	
-	String type=filename.substring(filename.lastIndexOf(".")+1,filename.length()).toLowerCase();
-	if(type.equals(temp))
-	{
-		return true;
-	}
-	else 
-	{
-		return false;
-	}
-}
 
 }
--- Word_Count/src/Readall.java
+++ Word_Count/src/Readall.java
@@ -10,15 +10,34 @@ import java.io.FileWriter;
 public class Readall   //递归读取文本文件
 {
 	    String outpath; //结果输出路径
+	    String temp;   //用户要求的后缀名
 	    
-	    public void test(String fileDir) 
+	    /**处理递归检索路径**/
+	    public void deal(String filepath)
+	    {
+	    	gettemp(filepath);  //获取用户要检索的文件后缀
+	    	if(filepath.matches("^\\*.*"))  //如果输入的文件路径形如"*.c"
+	    	{
+	    		filepath=".";               //直接搜索wc.exe所在文件夹
+	    	}
+	    	else
+	    	{
+	    		filepath=filepath.substring(0,filepath.lastIndexOf("\\*")).toLowerCase(); //否则获取"*.c"之前的路径
+	    	}
+	    	find(filepath);                 //遍历文件夹
+	    }
+	    	    
+	    /**递归检索文件并输出统计结果**/
+	    public void find(String fileDir) 
 	    {  
 	        List<File> fileList = new ArrayList<File>();  
 	        File file = new File(fileDir);  
 	        File[] files = file.listFiles();// 获取目录下的所有文件或文件夹  
+        	Calculate cal=new Calculate(); //生成统计类
+        	
 	        if (files == null) 
 	        {
-	        	// 如果目录为空，直接退出  
+	        	System.out.println("非文件夹路径！");
 	            return;  
 	        }  
 	        // 遍历目录下的所有文件  
@@ -30,16 +49,42 @@ public class Readall   //递归读取文本文件
 	            } 
 	            else if (f.isDirectory()) 
 	            {  
-	                test(f.getAbsolutePath());  
+	                find(f.getAbsolutePath());  
 	            }  
 	        }  
 	        
 	        for (File f1 : fileList) 
 	        {   
-	        	Calculate cal=new Calculate(); //生成统计类
+	        	if(match(f1.getName()))
+	        	{
 	        	String filepath=f1.getAbsolutePath(); //生成该文本文件绝对路径
 	        	cal.calfile(filepath);    //统计该文本文件字符数，单词数，行数
 	        	cal.writefile(f1, cal, outpath);  //将结果写入指定文件
+	        	}
 	        }  
 	    }	     
+	    
+	    
+	    /**对比文件后缀名是否符合检索要求**/
+	    public boolean match(String filename) 
+	    {
+	    	
+	    	String type=filename.substring(filename.lastIndexOf(".")+1,filename.length()).toLowerCase();
+	    	if(type.equals(temp))
+	    	{
+	    		return true;
+	    	}
+	    	else 
+	    	{
+	    		return false;
+	    	}
+	    }
+	    
+	    /**获得用户要检索的文件后缀名**/
+	    /**在主函数中获得用户输入的检索类型时使用**/
+	    public void gettemp(String filename) 
+	    {
+	    	temp=filename.substring(filename.lastIndexOf(".")+1,filename.length()).toLowerCase();
+	    }
+	    
 }
